2b3f621bc6d9f6a5f86ed6f8d24d19b9ebb69e97
Phil Rennie <phil@philrennie.co.uk>
2015-10-09 17:01:10 +0100

    Button countdown timer

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var lirc_client = require('lirc_client');
 var http = require('http');
 var express = require('express');
+var moment = require('moment');
+
+var now = moment();
+now.set({'minute':10, 'second':0});
 
 var app = express();
 var port = process.env.port || 8080;
@@ -12,14 +16,44 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 });
+
+io.on('connection', function(socket){
+  socket.emit('button', now.format("mm:ss"));
+});
+
+var Gpio = require('onoff').Gpio,
+  //led = new Gpio(14, 'out'),
+  button = new Gpio(4, 'in', 'both');
+
+var pressed = 0;
+
+function dec(){
+    if(pressed == 1){
+      now.subtract(10, 'ms');
+      io.sockets.emit('button', now.format("mm:ss"));
+      setTimeout(dec, 10);
+    }   
+}
+
+button.watch(function(err, value) {
+  //led.writeSync(value);
+  if(value == 1){
+     pressed = 1;
+     dec();
+  } else {
+    pressed = 0;
+  }
+});
+
+function exit() {
+  button.unexport();
+  process.exit();
+}
  
 try {
     lirc_client.connect("testone", true, "test1.lircrc",function(type, data, configFile){
-        //console.log("Type:", type);
-        //console.log("Data:", data);
         switch (type) {
             //case "rawdata":
-            //    console.log("Rawdata received:",data);
             //    break;
             case "data":
                 io.sockets.emit('button', data);
@@ -42,3 +76,4 @@ catch (err) {
     console.log("Error on connect:",err);
 }
 
+process.on('SIGINT', exit);
